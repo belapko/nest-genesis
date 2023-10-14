ba530d40b3d59627a2232f5e3f26cfde3c56dca5
belapko <belapko23@gmail.com>
2023-10-14 19:30:24 +0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://vue-genesis.vercel.app'],
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['X-Client-Id', 'Content-Type', 'Authorization'],
-    credentials: true,
+    origin: true,
+    // origin: ['https://vue-genesis.vercel.app'],
+    // methods: ['GET', 'POST', 'OPTIONS'],
+    // allowedHeaders: ['X-Client-Id', 'Content-Type', 'Authorization'],
+    // credentials: true,
   });
   await app.listen(3000);
 }
